28b434734eb3047d19f2cdedcc745fca9de7321f
Constantin Amurov <constantin.amurov@assist.ro>
2024-05-06 15:33:36 +0200

    Add Summary module + finish app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,20 @@ import { AppController } from './app.controller';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ReportService } from './report/report.service';
 import { ReportController } from './report/report.controller';
+import { SummaryController } from './summary/summary.controller';
+import { SummaryService } from './summary/summary.service';
+import { SummaryModule } from './summary/summary.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController,ReportController],
+  imports: [SummaryModule],
+  controllers: [AppController,ReportController, SummaryController],
   providers: [
     ReportService,
     {
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor,
     },
+    SummaryService,
   ],
 })
 export class AppModule {}
---
+++ src/summary/summary.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { SummaryController } from './summary.controller';
+import { SummaryService } from './summary.service';
+import { ReportService } from 'src/report/report.service';
+import { ReportModule } from 'src/report/report.module';
+
+@Module({controllers: [SummaryController], providers: [SummaryService], imports: [ReportModule]})
+export class SummaryModule {}
